607c71c0e5ffef7d855613aa05c9e65b2c4a3d86
Anton K <antko33@gmail.com>
2025-03-30 11:53:47 +0300

    Create routing file

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import asyncio
 
-from app.db import db_init
-from app.resolver import Resolver, load_dns, load_urls
+from app.db import db_close, db_init
+from app.resolver import Resolver, load_dns, load_urls, save_to_file
 
 
 async def main() -> None:
     await db_init()
 
     resolver = Resolver(await load_dns(), await load_urls())
-    _ = await resolver.resolve_all()
+    ips = await resolver.resolve_all()
+    save_to_file(ips)
+
+    await db_close()
 
 
 if __name__ == "__main__":
--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -1,13 +1,39 @@
+import os
+from pathlib import Path
+
 import pytest
 
-from app.resolver import Resolver
+from app.resolver import Resolver, save_to_file
 
 
 @pytest.mark.asyncio
 async def test_resolve_yt():
+    resolved_ips = await __resolve()
+
+    assert len(resolved_ips) > 0
+
+
+@pytest.mark.asyncio
+async def test_create_file():
+    resolved_ips = await __resolve()
+    filename = save_to_file(resolved_ips)
+
+    file = Path(filename)
+    assert file.exists()
+    assert file.is_file()
+
+    with open(filename, "r", encoding="utf-8") as f:
+        content = f.readlines()
+    assert len(content) > 0
+
+    if file.exists():
+        os.remove(filename)
+
+
+async def __resolve() -> set[str]:
     dns_list = {"Google": ["8.8.8.8", "8.8.4.4"], "Yandex": ["77.88.8.8", "77.88.8.1"]}
 
     resolver = Resolver(dns_list, {"youtube_mini": ["youtube.com"]})
     result = await resolver.resolve_all()
 
-    assert len(result) > 0
+    return result
