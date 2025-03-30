e7c24ec991e7fd74db19b15ffa8ae857316d7407
Anton K <antko33@gmail.com>
2025-03-30 13:20:32 +0300

    Setup bot

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import asyncio
+import os
+
+from dotenv import load_dotenv
 
 from app.db import db_close, db_init
 from app.resolver import Resolver, load_dns, load_urls, save_to_file
+from bot.bot import Bot
 
 
 async def main() -> None:
+    load_dotenv()
+    bot_token = os.getenv("BOT_TOKEN")
+    user_id = os.getenv("USER_ID")
+
     await db_init()
 
     resolver = Resolver(await load_dns(), await load_urls())
     ips = await resolver.resolve_all()
-    save_to_file(ips)
+    result_file = save_to_file(ips)
+
+    if result_file.strip():
+        bot = Bot(bot_token)
+        await bot.send_file(user_id, result_file)
 
     await db_close()
 
