db6f1637f1c76c2eaaf9c5f4d5f71d458071df2f
Anton K <antko33@gmail.com>
2025-03-31 21:26:56 +0300

    Deploy fix

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import asyncio
 import uvicorn
 from fastapi import FastAPI
 
-from app.config import API_HOST, API_PORT
+from app.config import API_PORT
 from app.db import db_init
 from app.routes import router
 
@@ -13,7 +13,7 @@ fastapi_app.include_router(router)
 
 def main():
     asyncio.run(db_init())
-    uvicorn.run(fastapi_app, host=API_HOST, port=API_PORT)
+    uvicorn.run(fastapi_app, host="0.0.0.0", port=API_PORT)
 
 
 if __name__ == "__main__":
